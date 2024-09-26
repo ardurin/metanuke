b38ec9e0ce63fea47037e30ed520ee3bc590379b
ardurin <ardurin@proton.me>
2024-09-26 18:46:49 +0100

    Add PDF tests

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #/!bin/sh
 directory=$(mktemp -d)
 processed=$(mktemp)
-for suite in ${1:-jpeg png}; do
+for suite in ${1:-jpeg pdf png}; do
 	i=1
 	for original in test/"${suite}"/*; do
 		target/debug/mwipe -o "${processed}" "${original}"
@@ -9,6 +9,10 @@ for suite in ${1:-jpeg png}; do
 			jpeginfo "${processed}" >/dev/null || echo "JPEG #${i}: failed"
 		elif [ "${suite}" = png ]; then
 			pngcheck "${processed}" >/dev/null || echo "PNG #${i}: failed"
+		elif [ "${suite}" = pdf ]; then
+			# Temporary hack until https://github.com/J-F-Liu/lopdf/issues/331 is fixed
+			sed -i 's|/Prev [0-9][0-9]*\([/>]\)|\1|g' "${processed}"
+			(qpdf --check "${processed}" >/dev/null 2>&1 && [ $(pdfinfo "${processed}" | wc -l) = 14 ]) || echo "PDF #${i}: failed"
 		fi
 		i=$((i + 1))
 	done
