d531544ab0636a285f0f838629373e2e3a658f2d
ardurin <ardurin@proton.me>
2024-10-02 22:28:33 +0200

    Read and filter PDF objects in one pass

--- src/metadata/pdf.rs
+++ src/metadata/pdf.rs
@@ -1,5 +1,5 @@
 use crate::error::Error;
-use lopdf::{Document, Object};
+use lopdf::{Document, Object, Reader};
 use std::io::{Read, Write};
 
 pub fn delete_metadata<R: Read, W: Write>(
@@ -8,29 +8,28 @@ pub fn delete_metadata<R: Read, W: Write>(
 ) -> Result<(), Error> {
 	let mut data = Vec::new();
 	source.read_to_end(&mut data)?;
-	let mut delete = vec![];
-	let mut document = Document::load_from(data.as_slice())?;
-	if let Ok(Object::Reference(reference)) = document.trailer.get(b"Info") {
-		delete.push(*reference);
+	let mut document = Reader {
+		buffer: &data,
+		document: Document::new(),
 	}
-	// Document::delete_object does not check the trailer for references to that object
-	document.trailer.remove(b"Info");
-	for (reference, object) in document.objects.iter_mut() {
-		match object.type_name() {
-			Ok("DocTimeStamp") | Ok("Metadata") | Ok("Sig") => {
-				delete.push(*reference);
-			}
-			_ => {
-				if let Ok(dictionary) = object.as_dict_mut() {
-					dictionary.remove(b"LastModified");
-					dictionary.remove(b"PieceInfo");
-				}
-			}
-		}
-	}
-	for identifier in delete {
+	.read(Some(evaluate))?;
+	if let Some(Object::Reference(identifier)) = document.trailer.remove(b"Info") {
 		document.delete_object(identifier);
 	}
 	document.save_to(destination)?;
 	Ok(())
+}
+
+fn evaluate(identifier: (u32, u16), object: &mut Object) -> Option<((u32, u16), Object)> {
+	match object.type_name() {
+		Ok("DocTimeStamp") | Ok("Metadata") | Ok("Sig") => None,
+		_ => {
+			if let Ok(dictionary) = object.as_dict_mut() {
+				dictionary.remove(b"LastModified");
+				dictionary.remove(b"Metadata");
+				dictionary.remove(b"PieceInfo");
+			}
+			Some((identifier, object.clone()))
+		}
+	}
 }
